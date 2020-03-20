4aaf6f22df3b77b73baf6f840a16607c5cccfe18
kunal <kunalgk1999@gmail.com>
2020-03-20 11:00:01 +0530

    list all dirs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 
 def main():
-    for root,d_names,f_names in os.walk("."):
-	print(root, d_names, f_names)
+	for root,d_names,f_names in os.walk("."):
+		print(root, d_names, f_names)
 
 if __name__ == "__main__":
-    main()
+ 	main()
