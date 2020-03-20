4a586be1d507bf734dddf011569a924db9d9b258
kunal <kunalgk1999@gmail.com>
2020-03-20 11:26:50 +0530

    added path

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import os
 
 def main():
-	for root,d_names,f_names in os.walk("."):
-		print(root, d_names, f_names)
+	path = os.environ["INPUT_PATH"]
+
+	_d_names = []
+	_f_names = []
+	failures = []
+	for root,d_names,f_names in os.walk(path):
+		for f_name in f_names:
+			if f_name not in _f_names:
+				_f_names.append(f_name)
+			elif f_name not in failures:
+				failures.append([root, f_name])
+
+	print(failures)
+
+	assert len(failures) == 0
 
 if __name__ == "__main__":
  	main()
